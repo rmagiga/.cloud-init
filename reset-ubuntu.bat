64c8b826372cbe67fcca14616d3cf67eea7ae006
rmagiga <rma_giga@yahoo.co.jp>
2024-12-03 19:57:42 +0900

    init

--- reset-ubuntu.bat
+++ reset-ubuntu.bat
@@ -4,7 +4,7 @@ wsl --unregister Ubuntu-24.04 > nul 2>&1
 wsl --install --distribution Ubuntu-24.04 --no-launch > nul 2>&1
 ubuntu2404.exe install --root
 ubuntu2404.exe run cloud-init status --wait
-wsl --terminate Ubuntu-24.04
 
-rem ubuntu2404.exe config --default-user ubuntu
+wsl -t Ubuntu-24.04
+rem wsl -d Ubuntu-24.04
 ubuntu2404.exe
